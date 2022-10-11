d3a3967345c4a9984a2cbe4d8e6b2d584c12e8f5
Sr.Skywalker <kamogelooliphant140@gmail.com>
2022-10-11 18:06:34 +0200

    change

--- index.js
+++ index.js
@@ -19,40 +19,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-/* your first API endpoint... 
+//* your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
-});*/
-
-// creating my timestamp request
-let responseObject = {}
-
-app.get('/api/:input', (request, response) =>{
-  let input = request.params.input
-
-  if (input.includes('-')) {
-    responseObject['unix'] = new Date(input).getTime()
-    responseObject['uct'] = new Date(input).toUTCString()   
-  }else{
-     /** parsig the string into an integer then storing it in the input variable */
-     input = parseInt(input)
-     responseObject['unix'] = new Date(input).getTime()
-     responseObject['uct'] = new Date(input).toUTCString()
-  }
-
-  if (!responseObject['unix'] || !responseObject['uct']) {
-    response.json({error: 'Invalid Date'})
-  } 
-
-  response.json(responseObject)
-});
-
-/* This is used for an empty url  just the time stamp without */
-app.get('/api/timestamp', (request, response) => {
-  responseObject['unix'] = new Date().getTime()
-  responseObject['uct'] = new Date().toUTCString()
-
-  response.json(responseObject) 
 });
 
 // listen for requests :)
@@ -60,3 +29,19 @@ var port = process.env.PORT || 3000;
 var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// creating my timestamp request
+
+let responseObject = {}
+
+app.get('/api/timestamp/:input', (req,res) => {
+  let input = request.params.input
+
+  if(input.includes('') || input.includes('-')){
+    responseObject['unix'] = new Date(input).getTime();
+  }
+
+  response.json(responseObject)
+})
+
+
