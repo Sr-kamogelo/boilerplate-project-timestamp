2c568a0432e57ab72d9e53d4ddba3242e71bf0d1
Sr.Skywalker <kamogelooliphant140@gmail.com>
2022-10-31 09:16:41 +0200

    init

--- index.js
+++ index.js
@@ -33,6 +33,29 @@ var listener = app.listen(port, function () {
 
 let responseObject = {}
 
+app.get('/api/:input', (request, response) => {
+  let input = request.params.input
+  
+  if(input.includes('-')){
+    /* Date String */
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }else{
+    /* Timestamp */
+    input = parseInt(input)
+    
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+  
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: 'Invalid Date'})
+  }
+  
+  
+  response.json(responseObject)
+})
+
 app.get('/api/', function(req, res){
   responseObject['unix'] = new Date().getTime(),
   responseObject['utc'] = new Date().toUTCString()
